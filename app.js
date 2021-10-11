bcbdcbec582523e8ba0850b7d96342d303e5edf6
brentleejohnson <brentleejohnson73@gmail.com>
2021-10-11 15:28:55 +0200

    Update

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://www.thenetninja.co.uk",
       showBooks: true,
-      title: "The Way of Kings",
-      author: "Brandon Sanderson",
-      age: 45,
+      books: [
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+        },
+      ],
     };
   },
   methods: {
